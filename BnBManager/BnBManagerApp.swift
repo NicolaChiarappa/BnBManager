18c2be3ee4a6ff63677528ca5f87e7e4096a0a05
Nick <nicolachiarappa2001@gmail.com>
2025-02-03 21:10:37 +0100

    Initial Commit

---
+++ BnBManager/BnBManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BnBManagerApp.swift
+//  BnBManager
+//
+//  Created by Nicola Chiarappa on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct BnBManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
